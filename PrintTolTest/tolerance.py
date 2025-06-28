abc953fae59a870ce4872b1f697f93d1e28cc29f
NamelessNanashi <53353250+NanashiTheNameless@users.noreply.github.com>
2025-06-27 23:28:10 -0500

    formatting tweaks

--- PrintTolTest/tolerance.py
+++ PrintTolTest/tolerance.py
@@ -1,7 +1,7 @@
 def calculate_tolerance(expected, measured):
     tolerances = {}
-    for axis, e, m in zip(['X', 'Y', 'Z'], expected, measured):
+    for axis, e, m in zip(["X", "Y", "Z"], expected, measured):
         signed = ((m - e) / e) * 100
         absolute = abs(signed)
-        tolerances[axis] = {'signed': signed, 'absolute': absolute}
+        tolerances[axis] = {"signed": signed, "absolute": absolute}
     return tolerances
