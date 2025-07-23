d82e97018b0085e285241dd92df32b42e99e5c21
NamelessNanashi <53353250+NanashiTheNameless@users.noreply.github.com>
2025-07-23 10:20:58 -0500

    modify localtest.sh

--- localtest.sh
+++ localtest.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 pipx install --force tox
-tox --verbose --recreate
-tox --verbose --recreate -e lint
-tox --verbose --recreate -e format
+tox --recreate
+tox --recreate -e lint
+tox --recreate -e format
